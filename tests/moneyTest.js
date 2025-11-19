e8c8bdc985a88e64a3c66728e2edfa34c63f3ff6
Nicholas Finefter <nfinefter@gmail.com>
2025-11-19 12:51:58 -0800

    Created test cases for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,29 @@
+import { formatCurrency} from "../scripts/utils/money.js";
+
+console.log('Test Suite: formatCurrency');
+
+if (formatCurrency(2095) === '20.95') {
+  console.log("All tests passed!");
+} else {
+  console.log("Some tests failed.");
+}
+if (formatCurrency(100) === '1.00') {
+  console.log("All tests passed!");
+} else {    
+  console.log("Some tests failed.");
+}
+if (formatCurrency(0) === '0.00') {
+  console.log("All tests passed!");
+} else {
+  console.log("Some tests failed.");
+}
+if (formatCurrency(123456) === '1234.56') {
+  console.log("All tests passed!");
+} else {
+  console.log("Some tests failed.");
+}
+if (formatCurrency(1000) === '10.00') {
+  console.log("All tests passed!");
+} else {
+  console.log("Some tests failed.");
+}
